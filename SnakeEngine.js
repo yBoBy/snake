8d8f213b771a6aca5f18e65cc83108c4e62bd310
yBoBy <mts96@web.de>
2020-06-15 23:29:42 +0200

    Gesture recognizer added

--- SnakeEngine.js
+++ SnakeEngine.js
@@ -5,25 +5,29 @@ import Constants from './Constants';
 import { GameLoop } from './GameLoop';
 import Head from './Head';
 import { GameEngine } from 'react-native-game-engine';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
-function SnakeEngine () {
+function SnakeEngine() {
     let boardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
     let engine = null;
-    const [speed, setSpeed] = useState({ xSpeed: 0, ySpeed: 0,});
-    
+    const [speed, setSpeed] = useState({ xSpeed: 0, ySpeed: 0, });
+
+
 
     return (
-        <GameEngine 
-          ref={(ref) => { engine = ref} }
-          style={{ width: boardSize, height: boardSize, flex: null, backgroundColor: '#ffffff'}}
-          systems={[ GameLoop ]}
-          entities={{
-            head: { position: [0,5], speed: speed ,size: Constants.CELL_SIZE, renderer: <Head />}
-          }}
-          
-      />
+        
+            <GameEngine
+                ref={(ref) => { engine = ref }}
+                style={{ width: boardSize, height: boardSize, flex: null, backgroundColor: '#ffffff' }}
+                systems={[GameLoop]}
+                entities={{
+                    head: { position: [0, 5], speed: speed, size: Constants.CELL_SIZE, renderer: <Head /> }
+                }}
+
+            />
+        
     );
 }
 
 
-export  { SnakeEngine };
+export { SnakeEngine };
