71383dc0fdfb00a8f5b1c7ea262be3af5822be91
yBoBy <mts96@web.de>
2020-06-15 22:00:53 +0200

    Added SnakeEngine.js

---
+++ SnakeEngine.js
@@ -0,0 +1,29 @@
+"use strict";
+import * as React from 'react';
+import { useState } from 'react';
+import Constants from './Constants';
+import { GameLoop } from './GameLoop';
+import Head from './Head';
+import { GameEngine } from 'react-native-game-engine';
+
+function SnakeEngine () {
+    let boardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
+    let engine = null;
+    const [speed, setSpeed] = useState({ xSpeed: 0, ySpeed: 0,});
+    
+
+    return (
+        <GameEngine 
+          ref={(ref) => { engine = ref} }
+          style={{ width: boardSize, height: boardSize, flex: null, backgroundColor: '#ffffff'}}
+          systems={[ GameLoop ]}
+          entities={{
+            head: { position: [0,5], speed: speed ,size: Constants.CELL_SIZE, renderer: <Head />}
+          }}
+          
+      />
+    );
+}
+
+
+export  { SnakeEngine };
