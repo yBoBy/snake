5ed46ec29b6bd7ae94a9010f579ccded5ac77852
yBoBy <mts96@web.de>
2020-06-17 00:49:19 +0200

    Swipe Steuerung implementiert (immernoch buggy)

--- SnakeEngine.js
+++ SnakeEngine.js
@@ -5,27 +5,52 @@ import Constants from './Constants';
 import { GameLoop } from './GameLoop';
 import Head from './Head';
 import { GameEngine } from 'react-native-game-engine';
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 
-function SnakeEngine() {
+const SnakeEngine = (props) => {
     let boardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
     let engine = null;
-    const [speed, setSpeed] = useState({ xSpeed: 0, ySpeed: 0, });
 
 
+    const config = {
+        velocityThreshold: 0.3,
+        directionalOffsetThreshold: 80
+      };
 
     return (
+        <>
         
-            <GameEngine
-                ref={(ref) => { engine = ref }}
-                style={{ width: boardSize, height: boardSize, flex: null, backgroundColor: '#ffffff' }}
-                systems={[GameLoop]}
-                entities={{
-                    head: { position: [0, 5], speed: speed, size: Constants.CELL_SIZE, renderer: <Head /> }
-                }}
+        <GameEngine
+            ref={(ref) => { engine = ref }}
+            style={{ width: boardSize, height: boardSize, flex: null, backgroundColor: '#ffffff' }}
+            systems={[GameLoop]}
+            entities={{
+                head: { position: [0, 5], xSpeed: 1, ySpeed: 0, size: Constants.CELL_SIZE, renderer: <Head /> }
+            }}
+        />
+        <GestureRecognizer
+            onSwipeRight={() => engine.dispatch({ type: "right"})}
+            onSwipeLeft={() => engine.dispatch({ type: "left"})}
+            onSwipeUp={() => engine.dispatch({ type: "up"})}
+            onSwipeDown={() => engine.dispatch({ type: "down"})}
+            config={config}
+            style={{
+                flex: 1,
+                position: 'absolute',
+                //backgroundColor: 'red',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                opacity: 0
+            }}
+        >
+        </GestureRecognizer>
+        
 
-            />
-        
+
+
+        </> 
     );
 }
 
