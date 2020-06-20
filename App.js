7e6238cbecca590af46aefa9efda48ef4201f0f5
Hannes Kiel <kielhannes@web.de>
2020-06-20 20:21:35 +0200

    Replaced Menu Buttons with TOpacities, tidied up code

--- App.js
+++ App.js
@@ -1,87 +1,87 @@
 'use strict';
 
 import * as React from 'react';
-import {useState} from 'react';
-import {Button, View, StyleSheet, Text, Image, StatusBar} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {block} from 'react-native-reanimated';
+import { useState } from 'react';
+import { Button, View, StyleSheet, Text, Image, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { block } from 'react-native-reanimated';
 import Constants from './Constants';
 import Head from './Head';
-import {SnakeEngine} from './SnakeEngine';
-import {GameLoop} from './GameLoop';
-import {GameEngine} from 'react-native-game-engine';
+import { SnakeEngine } from './SnakeEngine';
+import { GameLoop } from './GameLoop';
+import { GameEngine } from 'react-native-game-engine';
 import SettingsScreen from './SettingsScreen';
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 import { NavigationActions } from 'react-navigation';
 import Settings from './Settings'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 /*Test*/
 
-function MainMenu({navigation}) {
+function MainMenu({ navigation }) {
   // hide Statusbar when in game
   StatusBar.setHidden(true);
 
-  navigation.setOptions({
-    headerShown: true,
-    title: 'Settings',
-    headerTintColor: 'white',
-    headerStyle: {
-      backgroundColor: Settings.colorNavigationBar,
-    }
-  })
-
   return (
-    <View style={{width: '100%', flex: 1, justifyContent:"flex-start",  backgroundColor: "grey", padding: 5}}>
+    <View style={{ width: '100%', flex: 1, justifyContent: "flex-start", backgroundColor: Constants.COLOR_GENERAL_BACKGROUND, padding: 5 }}>
       {/* <Image
         source={require('./images/MainMenu/snake.gif')}
         style={{height: 300, width: 300, alignSelf: 'center', margin: 5}}
       /> */}
-      <Button
-        title="Start a New Game"
-        onPress={() => navigation.navigate(' ')}
-      />
-      <Button
-        title="Settings"
-        onPress={() => navigation.navigate('Settings')}
-      />
-      <Button
-        title="Update UI Colors"
-        onPress={() => navigation.setOptions({
-          headerShown: true,
-          title: 'Settings',
-          headerTintColor: 'white',
-          headerStyle: {
-            backgroundColor: Settings.colorNavigationBar,
-          }
-        })}
-      />
+      <View style={[{
+        margin: 5,
+        backgroundColor: Constants.COLOR_BUTTON_COLOR,
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: Constants.COLOR_BUTTON_BORDER,
+        alignItems: "stretch",
+      }]}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(' ')}
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: Constants.COLOR_BUTTON_TEXT, textAlign: 'center', margin: 5 }}>START A NEW GAME</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={[{
+        margin: 5,
+        backgroundColor: Constants.COLOR_BUTTON_COLOR,
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: Constants.COLOR_BUTTON_BORDER,
+        alignItems: "stretch",
+      }]}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Settings')}
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: Constants.COLOR_BUTTON_TEXT, textAlign: 'center', margin: 5 }}>SETTINGS</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-function GameScreen({navigation}) {
+function GameScreen({ navigation }) {
   return (
     <>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <SnakeEngine />
       </View>
     </>
   );
 }
 
-function showSettingsScreen({navigation}) {
-  navigation.setOptions({
-    headerShown: true,
-    title: 'Settings',
-    headerTintColor: 'white',
-    headerStyle: {
-      backgroundColor: Settings.colorNavigationBar,
-    }
-  })
+function showSettingsScreen({ navigation }) {
   return (
     <>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <SettingsScreen />
       </View>
     </>
@@ -99,7 +99,7 @@ function MyStack() {
           title: 'Snake - Main Menu',
           headerTintColor: 'white',
           headerStyle: {
-            backgroundColor: Settings.colorNavigationBar,
+            backgroundColor: Constants.COLOR_NAVIGATION_BAR,
           },
         }}
       />
@@ -109,8 +109,8 @@ function MyStack() {
           title: 'Settings',
           headerTintColor: 'white',
           headerStyle: {
-            backgroundColor: Settings.colorNavigationBar,
-          },
+            backgroundColor: Constants.COLOR_NAVIGATION_BAR,
+          }
         }}
       />
       <Stack.Screen
