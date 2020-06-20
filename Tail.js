72308101533401b48ca5dde1fdbbd43006c16f0c
yBoBy <mts96@web.de>
2020-06-20 23:44:33 +0200

    Tail fixed

--- Tail.js
+++ Tail.js
@@ -5,63 +5,173 @@ import Settings from './Settings';
 import _ from 'underscore';
 
 export default (Tail = props => {
-  const VERTICAL = {
-    width: props.size / 2,
-    height: props.size,
+  //Forms
+  const TOP_LEFT = 1;
+  const TOP_RIGHT = 2;
+  const BOTTOM_LEFT = 3;
+  const BOTTOM_RIGHT = 4;
+  const TOP_BOTTOM = 5;
+  const LEFT_RIGHT = 6;
+
+  const VERTICAL_PART = {
+    width: props.size * 0.5,
+    height: props.size * 0.75,
   };
-  const HORIZONTAL = {
-    width: props.size,
-    height: props.size / 2,
+  const HORIZONTAL_PART = {
+    width: props.size * 0.75,
+    height: props.size * 0.5,
   };
-  const CORNER = {width: null, height: null, top: null, left: null};
+
+  const getFormFromHead = (headDirection, current) => {
+    if (current.form === null) {
+      return LEFT_RIGHT;
+    }
+    if (_.isEqual(headDirection, Constants.UP)) {
+      if (_.isEqual(current.direction, Constants.UP)) return TOP_BOTTOM;
+      if (_.isEqual(current.direction, Constants.LEFT)) return TOP_RIGHT;
+      if (_.isEqual(current.direction, Constants.RIGHT)) return TOP_LEFT;
+    } else if (_.isEqual(headDirection, Constants.DOWN)) {
+      if (_.isEqual(current.direction, Constants.DOWN)) return TOP_BOTTOM;
+      if (_.isEqual(current.direction, Constants.LEFT)) return BOTTOM_RIGHT;
+      if (_.isEqual(current.direction, Constants.RIGHT)) return BOTTOM_LEFT;
+    } else if (_.isEqual(headDirection, Constants.LEFT)) {
+      if (_.isEqual(current.direction, Constants.LEFT)) return LEFT_RIGHT;
+      if (_.isEqual(current.direction, Constants.UP)) return BOTTOM_LEFT;
+      if (_.isEqual(current.direction, Constants.DOWN)) return TOP_LEFT;
+    } else if (_.isEqual(headDirection, Constants.RIGHT)) {
+      if (_.isEqual(current.direction, Constants.RIGHT)) return LEFT_RIGHT;
+      if (_.isEqual(current.direction, Constants.UP)) return BOTTOM_RIGHT;
+      if (_.isEqual(current.direction, Constants.DOWN)) return TOP_RIGHT;
+    }
+  };
+
+  const calculateViewParameters = (basePosition, form = null) => {
+    let leftNoPadding = basePosition[0] * props.size;
+    let topNoPadding = basePosition[1] * props.size;
+    let leftPadding = leftNoPadding + props.size * 0.25;
+    let topPadding = topNoPadding + props.size * 0.25;
+    let part1 = {left: null, top: null, size: null};
+    let part2 = {left: null, top: null, size: null};
+
+    switch (form) {
+      case TOP_LEFT:
+        part1.left = leftPadding;
+        part1.top = topNoPadding;
+        part2.left = leftNoPadding;
+        part2.top = topPadding;
+        part1.size = VERTICAL_PART;
+        part2.size = HORIZONTAL_PART;
+        break;
+      case TOP_RIGHT:
+        part1.left = leftPadding;
+        part1.top = topNoPadding;
+        part2.left = leftPadding;
+        part2.top = topPadding;
+        part1.size = VERTICAL_PART;
+        part2.size = HORIZONTAL_PART;
+        break;
+      case BOTTOM_LEFT:
+        part1.left = leftPadding;
+        part1.top = topPadding;
+        part2.left = leftNoPadding;
+        part2.top = topPadding;
+        part1.size = VERTICAL_PART;
+        part2.size = HORIZONTAL_PART;
+        break;
+      case BOTTOM_RIGHT:
+        part1.left = leftPadding;
+        part1.top = topPadding;
+        part2.left = leftPadding;
+        part2.top = topPadding;
+        part1.size = VERTICAL_PART;
+        part2.size = HORIZONTAL_PART;
+        break;
+      case LEFT_RIGHT:
+        part1.left = leftNoPadding;
+        part1.top = topPadding;
+        part2.left = leftPadding;
+        part2.top = topPadding;
+        part1.size = HORIZONTAL_PART;
+        part2.size = HORIZONTAL_PART;
+        break;
+      case TOP_BOTTOM:
+        part1.left = leftPadding;
+        part1.top = topPadding;
+        part2.left = leftPadding;
+        part2.top = topNoPadding;
+        part1.size = VERTICAL_PART;
+        part2.size = VERTICAL_PART;
+        break;
+      default:
+        console.log(
+          'Critical Error @ Tail.calculateViewParameters SWITCH_CASE',
+        );
+    }
+
+    return {part1: part1, part2: part2};
+  };
+
   let tailList = [];
-  let leftVal = null;
-  let topVal = null;
+  let partValues = null;
 
   for (let i = props.elements.length - 1; i >= 0; i--) {
     let cur = props.elements[i];
     let prev = null;
+    //Restliche Tail-Elemente
     if (i > 0) {
       prev = props.elements[i - 1];
+      cur.isCornerPart = prev.isCornerPart;
       cur.position = prev.position;
-      if (prev.orientation === null) {
-        cur.orientation = HORIZONTAL;
+      if (prev.form === null) {
+        //Set LEFT_RIGHT (Horizontal), because form of initial element is 'null'
+        cur.form = LEFT_RIGHT;
       } else {
-        cur.orientation = prev.orientation;
+        cur.form = prev.form;
       }
       cur.containsApple = prev.containsApple;
-    } else {
-      // cur.position = JSON.parse(JSON.stringify(props.headPosition));
-      cur.position = props.headPosition;
-      if (
-        _.isEqual(props.headDirection, Constants.RIGHT) ||
-        _.isEqual(props.headDirection, Constants.LEFT)
-      ) {
-        cur.orientation = HORIZONTAL;
-      } else {
-        cur.orientation = VERTICAL;
-      }
+
+      partValues = calculateViewParameters(cur.position, cur.form);
     }
-    console.log(`${i}: ${Object.values(cur.orientation)}`);
-    if (_.isEqual(cur.orientation, HORIZONTAL)) {
-      leftVal = props.size * cur.position[0];
-      topVal = props.size * cur.position[1] + 0.25 * props.size;
-      console.log(`Tail(${i}): ${topVal}`);
-    } else {
-      leftVal = props.size * cur.position[0] + 0.25 * props.size;
-      topVal = props.size * cur.position[1];
+
+    //Tail-Element direkt nach Kopf
+    else {
+      if (props.headDirection === null) {
+        props.headDirection = Object.values(Constants.RIGHT);
+      }
+
+      cur.position = props.headPosition;
+
+      cur.form = getFormFromHead(props.headDirection, cur);
+
+      partValues = calculateViewParameters(
+        Object.values(cur.position),
+        cur.form,
+      );
+
+      cur.direction = Object.values(props.headDirection);
     }
 
     tailList.push(
       <View
-        key={i}
+        key={'Part1.' + i}
         style={{
-          width: cur.orientation.width,
-          height: cur.orientation.height,
-          backgroundColor: Settings.colorSnakeTail,
           position: 'absolute',
-          left: leftVal,
-          top: topVal,
+          backgroundColor: Settings.colorSnakeTail,
+          width: partValues.part1.size.width,
+          height: partValues.part1.size.height,
+          left: partValues.part1.left,
+          top: partValues.part1.top,
+        }}
+      />,
+      <View
+        key={'Part2.' + i}
+        style={{
+          position: 'absolute',
+          backgroundColor: Settings.colorSnakeTail,
+          width: partValues.part2.size.width,
+          height: partValues.part2.size.height,
+          left: partValues.part2.left,
+          top: partValues.part2.top,
         }}
       />,
     );
