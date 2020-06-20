5b34d3b40e343b85aa929353baa91469c511aaeb
Hannes Kiel <kielhannes@web.de>
2020-06-20 02:27:55 +0200

    Modified Bettons in Settings, implemented change of gamespeed, buggy color changing

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import {useState} from 'react';
-import {Button, View, StyleSheet, Text, Image} from 'react-native';
+import {Button, View, StyleSheet, Text, Image, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {block} from 'react-native-reanimated';
@@ -13,13 +13,26 @@ import {GameLoop} from './GameLoop';
 import {GameEngine} from 'react-native-game-engine';
 import SettingsScreen from './SettingsScreen';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import { NavigationActions } from 'react-navigation';
+import Settings from './Settings'
 
 /*Test*/
 
 function MainMenu({navigation}) {
-  let Image_Http_URL = {};
+  // hide Statusbar when in game
+  StatusBar.setHidden(true);
+
+  navigation.setOptions({
+    headerShown: true,
+    title: 'Settings',
+    headerTintColor: 'white',
+    headerStyle: {
+      backgroundColor: Settings.colorNavigationBar,
+    }
+  })
+
   return (
-    <View style={{width: '100%', justifyContent: 'center'}}>
+    <View style={{width: '100%', flex: 1, justifyContent:"flex-start",  backgroundColor: "grey", padding: 5}}>
       {/* <Image
         source={require('./images/MainMenu/snake.gif')}
         style={{height: 300, width: 300, alignSelf: 'center', margin: 5}}
@@ -31,6 +44,17 @@ function MainMenu({navigation}) {
       <Button
         title="Settings"
         onPress={() => navigation.navigate('Settings')}
+      />
+      <Button
+        title="Update UI Colors"
+        onPress={() => navigation.setOptions({
+          headerShown: true,
+          title: 'Settings',
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: Settings.colorNavigationBar,
+          }
+        })}
       />
     </View>
   );
@@ -47,6 +71,14 @@ function GameScreen({navigation}) {
 }
 
 function showSettingsScreen({navigation}) {
+  navigation.setOptions({
+    headerShown: true,
+    title: 'Settings',
+    headerTintColor: 'white',
+    headerStyle: {
+      backgroundColor: Settings.colorNavigationBar,
+    }
+  })
   return (
     <>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -61,23 +93,37 @@ const Stack = createStackNavigator();
 function MyStack() {
   return (
     <Stack.Navigator initialRouteName="Main Menu">
-      <Stack.Screen name="Main Menu" component={MainMenu} />
-      <Stack.Screen name="Settings" component={showSettingsScreen} />
+      <Stack.Screen name="Main Menu" component={MainMenu}
+        options={{
+          headerShown: true,
+          title: 'Snake - Main Menu',
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: Settings.colorNavigationBar,
+          },
+        }}
+      />
+      <Stack.Screen name="Settings" component={showSettingsScreen}
+        options={{
+          headerShown: true,
+          title: 'Settings',
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: Settings.colorNavigationBar,
+          },
+        }}
+      />
       <Stack.Screen
         name=" "
         component={GameScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false
+        }}
       />
     </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  MainMenuButtonText: {
-    fontFamily: 'Cochin',
-    textAlign: 'auto',
-  },
-});
 
 export default function App() {
   return (
